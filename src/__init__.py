7217582fa9bf3456218dd27fbf77e5acb3e26d2e
Tom Lever <thomas.lever.business@gmail.com>
2025-05-15 21:07:23 -0400

    Add imports to src/data_processing/__init__.py.

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,4 @@ Source code for patient data analysis and integration.
 
 This package contains modules for processing, analyzing, and visualizing
 clinical, molecular, and single-cell data from patient samples.
-""" 
+"""
--- src/data_processing/__init__.py
+++ src/data_processing/__init__.py
@@ -13,6 +13,9 @@ __version__ = "1.0.0"
 
 # Make key functions available at package level
 try:
-    from .utils import create_map_from_qc
+    from .utils import create_map_from_qc, clean_id_string, load_csv
+    from .clinical import load_clinical_table
+    from .molecular import merge_molecular_qc
+    from .single_cell import integrate_single_cell_scores
 except ImportError:
     pass  # Allow partial imports if some modules aren't available 
